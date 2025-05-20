613f2c42847bb9284b852682047e328434d33396
mUnger09 <munger110078@muhsdstudents.org>
2025-05-20 16:22:00 -0700

    Bugfixes, minor changes in game logic, and added comments

--- Player.java
+++ Player.java
@@ -1,11 +1,15 @@
+//Matias Unger-Ramirez
+//05.19.25
+//My program is a survival/resource management game. 
+
 import java.util.HashMap;
 import java.util.Random;
 
 public class Player {
-    HashMap<String, Integer> status = new HashMap();
+    HashMap<String, Integer> status = new HashMap<String,  Integer>();
     Random rand = new Random();
 
-    public Player() {
+    public Player() {//Initializes Player with the following stats
         status.put("Health", 80);
         status.put("Hunger", 50);
         status.put("Morale", 50);
@@ -15,9 +19,13 @@ public class Player {
     }
 
     public void scavenge() { //get some food and ammo
-        System.out.println("You search the area.");
-        setStatus("Food", getStatus("Food") + 1 + rand.nextInt(2));
-        setStatus("Ammo", getStatus("Ammo") + 2 + rand.nextInt(3));
+        int newAmmo = 2 + rand.nextInt(3);
+        int newFood = 1 + rand.nextInt(2);
+        System.out.println("You search the area and find: ");
+        System.out.println(newFood + " food");
+        System.out.println(newAmmo + " ammo");
+        setStatus("Food", getStatus("Food") + newFood);
+        setStatus("Ammo", getStatus("Ammo") + newAmmo);
         setStatus("Morale", getStatus("Morale") + 5);
     }
     public void rest() { //increase health and morale by a small bit
@@ -27,23 +35,19 @@ public class Player {
         setStatus("Food", getStatus("Food") - 1); 
     }
     public void fortify() { //use small amount of food, increase fortifications 
-        if (getStatus("Food") >= 1) {
-            System.out.println("You reinforce your shelter.");
-            setStatus("Fortifications", getStatus("Fortifications") + 2);
-            setStatus("Food", getStatus("Food") - 1);
-        } else {
-            System.out.println("You're too hungry to do this right now.");
-        }
+        System.out.println("You reinforce your shelter.");
+        setStatus("Fortifications", getStatus("Fortifications") + 2);
+        setStatus("Food", getStatus("Food") - 1);
     }
-    public void setStatus(String playerStatus, int newValue) {
+    public void setStatus(String playerStatus, int newValue) {//Sets a specific player status
         status.put(playerStatus, newValue);
     }
 
-    public int getStatus(String playerStatus) {
+    public int getStatus(String playerStatus) {//Returns a specific status
         return status.get(playerStatus);
     }
 
-    public void getPlayerStatus() {
+    public void getPlayerStatus() {//Prints out every status of Player
         System.out.println("Health: " + getStatus("Health"));
         System.out.println("Hunger: " + getStatus("Hunger"));
         System.out.println("Morale: " + getStatus("Morale"));
